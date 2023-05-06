374ce38fd8909de3723affd63c60d39cefdb2237
filipe-m-cunha <filipe.miranda.cunha@tecnico.ulisboa.pt>
2023-05-06 21:22:57 +0200

    Added 13 img

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,6 @@ import DragAndDrop from './DragAndDrop';
 import React, { useState } from "react";
 
 function App() {
-
   const [selectedDisease, setSelectedDisease] = useState("Acne");
 
   const handleChange = (event) => {
@@ -14,25 +13,24 @@ function App() {
     setSelectedDisease(selectedDisease);
   };
 
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
-          <div className="logo-container">
-            <img src={logo} alt="Logo" className="logo" style={{width: '200px', height: '200px'}}/>
-          </div>
-          <h1 className="title">Drag and Drop Image Upload</h1>
-          <div className="disease-container">
-            <label htmlFor="disease-select" className="label">Select Disease:</label>
-            <select id="disease-select" value={selectedDisease} onChange={handleChange} className="select">
-              <option value="Acne">Acne</option>
-              <option value="Baldness">Baldness</option>
-              <option value="Option 3">Option 3</option>
-              <option value="Option 4">Option 4</option>
-              <option value="Option 5">Option 5</option>
-            </select>
-          </div>
-          <DragAndDrop />
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <h1 className="heading">Drag and Drop Image Upload</h1>
+        <DragAndDrop />
+        <div className="select-container">
+          <label htmlFor="disease-select" className="label">Select Disease:</label>
+          <select id="disease-select" value={selectedDisease} onChange={handleChange} className="select">
+            <option value="Acne">Acne</option>
+            <option value="Baldness">Baldness</option>
+            <option value="Option 3">Option 3</option>
+            <option value="Option 4">Option 4</option>
+            <option value="Option 5">Option 5</option>
+          </select>
         </div>
       </header>
     </div>
